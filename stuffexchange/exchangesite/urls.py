aa2a49163db8d8ce778ee19c1130313c13e8153c
Starcoding <fagost32@gmail.com>
2021-09-29 22:17:34 +0300

    Created new app. Added some URL's and empty views

---
+++ stuffexchange/exchangesite/urls.py
@@ -0,0 +1,17 @@
+from os import name
+from django.urls import path
+
+from .views import show_goods, show_good, show_user, create_exchange, show_offers
+
+
+app_name = "exchangesite"
+
+urlpatterns = [
+    path('', show_goods, name='index'),
+    path('user/<int:id>', show_user, name='user'),
+    path('good/<int:id>', show_good, name='good'),
+    path('offers/', show_offers, name='offers'),
+    path('exchange/', create_exchange, name='exchange'),
+    # path('login/', login, name='login'),
+    # path('register/', register, name='register'),
+]
--- stuffexchange/exchangesite/views.py
+++ stuffexchange/exchangesite/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import views as auth_views
 
-# Create your views here.
+
+def show_goods(request):
+    return HttpResponse('<h1>Это главная страница!</h1>')
+
+
+def show_good(request):
+    return HttpResponse('<h1>Это страница вещи!</h1>')
+
+
+def show_user(request):
+    return HttpResponse('<h1>Это страница юзера с товарами!</h1>')
+
+
+def create_exchange(request):
+    return HttpResponse('<h1>Здесь создаём предложение для обмена вещи!</h1>')
+
+
+def show_offers(request):
+    return HttpResponse('<h1>Здесь показываем какие у нас есть предложения по обмену вещей!</h1>')
