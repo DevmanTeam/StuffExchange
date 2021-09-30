a2bff3a37b75d8e4fd8964d3f0f20915fe545dcd
Fiskless <Fiskless@gmail.com>
2021-09-30 18:01:11 +0500

    Add models to app and registrate it in admin page

--- stuffexchange/exchangesite/admin.py
+++ stuffexchange/exchangesite/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from exchangesite.models import CustomUser,\
+    Good,\
+    Gallery,\
+    ExchangeFromUserToUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
+
+
+class GalleryInline(admin.TabularInline):
+    model = Gallery
+
+
+@admin.register(Good)
+class GoodAdmin(admin.ModelAdmin):
+    inlines = [GalleryInline]
+
+
+@admin.register(ExchangeFromUserToUser)
+class ExchangeFromUserToUserAdmin(admin.ModelAdmin):
+    pass
+
+
